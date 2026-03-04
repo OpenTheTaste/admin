619994a0b6565a994d878e8219d921006373f737
joooii <3098k@naver.com>
2026-03-04 16:42:08 +0900

    [OT-183] [FEAT]: 숏폼 API 호출부 구현

---
+++ src/entities/shorts/apis/getShortsApi.ts
@@ -0,0 +1,60 @@
+import { api } from "@/shared/api";
+import {
+  ApiResponse,
+  BasePaginationParams,
+  Category,
+  PageInfo,
+  PublicStatus,
+} from "@shared/types";
+
+export interface ShortsListItem {
+  mediaId: number;
+  posterUrl: string;
+  title: string;
+  publicStatus?: PublicStatus;
+  uploadedDate: string;
+}
+
+export interface ShortsListResponse {
+  pageInfo: PageInfo;
+  dataList: ShortsListItem[];
+}
+
+export interface GetShortsListParams extends BasePaginationParams {
+  page: number;
+  size: number;
+  searchWord?: string;
+  publicStatus?: PublicStatus;
+}
+
+// 숏폼 리스트 조회 API
+export const getShortsListApi = async (params: GetShortsListParams) => {
+  const res = await api.get<ApiResponse<ShortsListResponse>>("/short-forms", {
+    params,
+  });
+  return res.data.data;
+};
+
+export interface ShortsDetailResponse {
+  shortFormId: number;
+  posterUrl: string;
+  title: string;
+  description: string;
+  originContentsTitle: string;
+  uploaderNickname: string;
+  duration: number;
+  videoSize: number;
+  categoryName: Category;
+  tagNameList: string[];
+  publicStatus: PublicStatus;
+  bookmarkCount: number;
+  uploadedDate: string;
+}
+
+// 숏폼 상세 조회 API
+export const getShortsDetailApi = async (mediaId: number) => {
+  const res = await api.get<ApiResponse<ShortsDetailResponse>>(
+    `/short-forms/${mediaId}`,
+  );
+  return res.data.data;
+};
