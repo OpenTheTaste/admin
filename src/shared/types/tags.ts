5327a30e90a96cb4a0379343aece46b3d6fa6e41
joooii <3098k@naver.com>
2026-03-05 20:19:16 +0900

    [OT-192] [FIX]: 들어올 태그 API에 맞게 type 수정

--- src/shared/types/tags.ts
+++ src/shared/types/tags.ts
@@ -1,19 +1,35 @@
-import { Category } from "@shared/types";
+export interface Tag {
+  tagId: number;
+  name: string;
+}
 
-export const TAGS: Record<Category, string[]> = {
-  영화: ["로맨스", "사극", "액션", "코미디", "호러", "SF", "뮤지컬"],
-  드라마: [
-    "로맨스",
-    "코미디",
-    "가족",
-    "의학",
-    "법정",
-    "역사",
-    "스릴러",
-    "판타지",
+export const TAGS: Record<number, Tag[]> = {
+  // 드라마, 영화, 예능, 다큐
+  1: [
+    { tagId: 1, name: "로맨스" },
+    { tagId: 2, name: "스릴러" },
+    { tagId: 3, name: "추리" },
+    { tagId: 4, name: "법정" },
+    { tagId: 5, name: "의학" },
   ],
-  예능: ["코미디", "리얼리티", "토크쇼", "서바이벌", "여행", "연예"],
-  다큐: ["역사", "과학", "환경", "사회"],
-  뉴스: ["정치", "사회", "국제", "연예"],
-  스포츠: ["축구", "야구", "농구", "배구", "골프", "기타"],
+  2: [
+    { tagId: 6, name: "액션" },
+    { tagId: 7, name: "SF" },
+    { tagId: 8, name: "코미디" },
+    { tagId: 9, name: "공포" },
+    { tagId: 10, name: "판타지" },
+  ],
+
+  3: [
+    { tagId: 11, name: "토크쇼" },
+    { tagId: 12, name: "리얼리티" },
+    { tagId: 13, name: "음악" },
+    { tagId: 14, name: "먹방" },
+  ],
+  4: [
+    { tagId: 15, name: "자연" },
+    { tagId: 16, name: "역사" },
+    { tagId: 17, name: "과학" },
+    { tagId: 18, name: "사회" },
+  ],
 };
