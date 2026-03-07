d661c29ed0e83c62446fa2b818a0ab33226e9a13
hyunw-kang <vpffp368@naver.com>
2026-03-08 01:47:07 +0900

    [OT-213] [FEAT]: 시리즈 상세조회 API 세팅

---
+++ src/entities/series/apis/getSeriesDetailApi.ts
@@ -0,0 +1,23 @@
+import { api } from "@/shared/api";
+import { ApiResponse, Category, PublicStatus } from "@shared/types";
+
+export interface SeriesDetail {
+  seriesId: number;
+  title: string;
+  description: string;
+  categoryName: Category;
+  tagNameList: string[];
+  publicStatus: PublicStatus;
+  uploaderNickname: string;
+  bookmarkCount: number;
+  actors: string;
+  posterUrl: string;
+  thumbnailUrl: string;
+}
+
+export const getSeriesDetailApi = async (mediaId: number) => {
+  const res = await api.get<ApiResponse<SeriesDetail>>(
+    `admin/series/${mediaId}`,
+  );
+  return res.data.data;
+};
--- src/entities/series/apis/index.ts
+++ src/entities/series/apis/index.ts
@@ -1,2 +1,4 @@
 export { getSeriesListApi } from "./getSeriesApi";
 export type { SeriesListItem, SeriesListResponse, GetSeriesListParams } from "./getSeriesApi";
+export { getSeriesDetailApi } from "./getSeriesDetailApi";
+export type { SeriesDetail } from "./getSeriesDetailApi";
