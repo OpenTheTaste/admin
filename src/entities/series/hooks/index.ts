73abafe1745412764708a8a806b317a4b84eb184
hyunw-kang <vpffp368@naver.com>
2026-03-08 01:35:03 +0900

    [OT-213] [FEAT]: 스크롤 시리즈 리스트 훅 추가

---
+++ src/entities/series/hooks/index.ts
@@ -0,0 +1,1 @@
+export { useInfiniteSeriesList } from "./useSeriesList";
---
+++ src/entities/series/hooks/useSeriesList.ts
@@ -0,0 +1,39 @@
+import { useInfiniteScroll } from "@/shared/hooks";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { SeriesListItem, getSeriesListApi } from "@entities/series/apis";
+
+interface UseInfiniteSeriesListParams {
+  size?: number;
+  searchWord?: string;
+}
+
+export const useInfiniteSeriesList = ({
+  size = 10,
+  searchWord,
+}: UseInfiniteSeriesListParams) => {
+  const query = useInfiniteQuery({
+    queryKey: ["series", "list", { size, searchWord }],
+    queryFn: ({ pageParam = 0 }) =>
+      getSeriesListApi({
+        page: pageParam as number,
+        size,
+        searchWord: searchWord || undefined,
+      }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => {
+      const { currentPage, totalPage } = lastPage.pageInfo;
+      return currentPage + 1 < totalPage ? currentPage + 1 : undefined;
+    },
+  });
+
+  const { observerRef } = useInfiniteScroll({
+    hasNextPage: query.hasNextPage,
+    isFetchingNextPage: query.isFetchingNextPage,
+    fetchNextPage: query.fetchNextPage,
+  });
+
+  const seriesList: SeriesListItem[] =
+    query.data?.pages.flatMap((page) => page.dataList) ?? [];
+
+  return { ...query, seriesList, observerRef };
+};
