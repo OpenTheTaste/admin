51047f53ab8bb0c84295e53a0596c0c3b4a4a51e
hyunw-kang <vpffp368@naver.com>
2026-03-08 15:53:14 +0900

    [OT-213] [FEAT]: 시리즈 수정 API 세팅

--- src/entities/series/apis/index.ts
+++ src/entities/series/apis/index.ts
@@ -4,3 +4,5 @@ export { getSeriesDetailApi } from "./getSeriesDetailApi";
 export type { SeriesDetail } from "./getSeriesDetailApi";
 export { uploadSeriesApi } from "./uploadSeriesApi";
 export type { UploadSeriesRequest, UploadSeriesResponse } from "./uploadSeriesApi";
+export { fixSeriesApi } from "./fixSeriesApi";
+export type { FixSeriesRequest, FixSeriesResponse } from "./fixSeriesApi";
