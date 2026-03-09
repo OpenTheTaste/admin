75577cdf8f5a53ad389a21c503a0ffbd53891428
joooii <3098k@naver.com>
2026-03-10 00:27:51 +0900

    [OT-241] [FEAT]: 시리즈 제목 조회 API hooks 구현

---
+++ src/entities/video-contents/hooks/useSeriesTitle.ts
@@ -0,0 +1,41 @@
+import { useInfiniteScroll } from "@/shared/hooks";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import {
+  SeriesTitleItem,
+  getSeriesTitleApi,
+} from "@entities/video-contents/apis";
+
+interface UseInfiniteSeriesTitleParams {
+  size?: number;
+  searchWord?: string;
+}
+export const useInfiniteSeriesTitle = ({
+  size = 10,
+  searchWord,
+}: UseInfiniteSeriesTitleParams) => {
+  const query = useInfiniteQuery({
+    queryKey: ["series", "title", { size, searchWord }],
+    queryFn: ({ pageParam = 0 }) =>
+      getSeriesTitleApi({
+        page: pageParam as number,
+        size,
+        searchWord: searchWord || undefined,
+      }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => {
+      const { currentPage, totalPage } = lastPage.pageInfo;
+      return currentPage + 1 < totalPage ? currentPage + 1 : undefined;
+    },
+  });
+
+  const { observerRef } = useInfiniteScroll({
+    hasNextPage: query.hasNextPage,
+    isFetchingNextPage: query.isFetchingNextPage,
+    fetchNextPage: query.fetchNextPage,
+  });
+
+  const seriesTitles: SeriesTitleItem[] =
+    query.data?.pages.flatMap((page) => page.dataList) ?? [];
+
+  return { ...query, seriesTitles, observerRef };
+};
