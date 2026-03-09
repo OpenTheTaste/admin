8c1d25f7d1525137e60ede1d8fad37cdd63d4b0a
joooii <3098k@naver.com>
2026-03-09 15:51:12 +0900

    [OT-192] [FEAT]: 카테고리, 태그 뱃지 색상 추가

--- src/entities/tag/constants/tagColor.ts
+++ src/entities/tag/constants/tagColor.ts
@@ -3,7 +3,6 @@ export const CATEGORY_DOT_COLOR: Record<number, string> = {
   2: "bg-ot-red",
   3: "bg-ot-orange",
   4: "bg-ot-mint",
-  // FIXME: 카테고리 추가되면 여기에 추가하기
-  // 뉴스: "bg-ot-green",
-  // 스포츠: "bg-ot-blue",
+  5: "bg-ot-green",
+  6: "bg-ot-blue",
 };
