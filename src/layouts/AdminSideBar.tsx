f0758f31275dd70cc27d33a94101e40ff48729bb
joooii <3098k@naver.com>
2026-03-17 13:52:41 +0900

    [OT-337] [FIX]: 유저 정보 반응형 UI 구현

--- src/layouts/AdminSideBar.tsx
+++ src/layouts/AdminSideBar.tsx
@@ -77,7 +77,6 @@ export const AdminSideBar = () => {
           O+T 관리
         </span>
       </Link>
-
       <nav className="flex flex-col px-3">
         {filteredMenus.map((menu) => {
           const isActive = pathname.startsWith(menu.href);
@@ -101,12 +100,15 @@ export const AdminSideBar = () => {
         })}
       </nav>
 
-      <div className="mt-auto py-3 border-t border-ot-gray-600 px-4 flex items-center justify-between">
-        <div>
+      <div className="mt-auto py-3 border-t border-ot-gray-600 px-4 flex items-center justify-between min-h-15">
+        <div className="hidden md:block">
           <p className="font-semibold text-ot-text text-md">{nickname}</p>
           <p className="text-ot-placeholder text-sm">{email}</p>
         </div>
-        <button className="cursor-pointer" onClick={handleLogout}>
+        <button
+          className="cursor-pointer md:ml-0 ml-auto"
+          onClick={handleLogout}
+        >
           <LogOut
             className="stroke-ot-text hover:stroke-ot-gray-600"
             size={22}
