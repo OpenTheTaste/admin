cae9a9c5174bb2e1c62de62890e1b1c192e16766
RyuJaeHwi <yujaehwi02@gmail.com>
2026-03-18 16:53:25 +0900

    [OT-348][FIX]: shortsApi.ts -> postshortsApi.ts로 이름 변경

--- src/entities/shorts/apis/updateShortsApi.ts
+++ src/entities/shorts/apis/updateShortsApi.ts
@@ -1,7 +1,7 @@
 import { api } from "@shared/api";
 import { END_POINTS } from "@shared/constants";
 import { ApiResponse, PublicStatus } from "@shared/types";
-import { UploadShortsResponse } from "./shortsApi";
+import { UploadShortsResponse } from "./postshortsApi";
 
 export interface UpdateShortsRequest {
   originId: number;
