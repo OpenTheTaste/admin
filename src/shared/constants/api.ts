716447c0615faaf0883b76741e891f52c88843ec
RyuJaeHwi <yujaehwi02@gmail.com>
2026-03-18 13:08:31 +0900

    [OT-346][FIX]: 숏폼 관리 페이지 엔드포인트 상수화

--- src/shared/constants/api.ts
+++ src/shared/constants/api.ts
@@ -9,7 +9,7 @@ export const END_POINTS = {
   SERIES_UPLOAD: "admin/series/upload",
   SERIES_FIX: (seriesId: number) => `admin/series/${seriesId}/upload`,
 
-  // 콘텐츠
+  // 콘텐츠 관리 페이지
   CONTENTS: "/admin/contents",
   CONTENTS_DETAIL: (mediaId: number) => `/admin/contents/${mediaId}`,
   CONTENTS_UPLOAD: "/admin/contents/upload",
@@ -17,6 +17,16 @@ export const END_POINTS = {
     `/admin/contents/${contentsId}/upload`,
   CONTENTS_MULTIPART_COMPLETE: (contentsId: number) =>
     `/admin/contents/${contentsId}/upload/complete`,
+
+  // 숏폼 관리 페이지
+  SHORT_FORMS: "/short-forms",
+  SHORT_FORMS_DETAIL: (mediaId: number) => `/short-forms/${mediaId}`,
+  SHORT_FORMS_UPLOAD: "/short-forms/upload",
+  SHORT_FORMS_UPDATE: (shortformId: number) =>
+    `/short-forms/${shortformId}/upload`,
+  SHORT_FORMS_MULTIPART_COMPLETE: (shortformId: number) =>
+    `/short-forms/${shortformId}/upload/complete`,
+  SHORT_FORMS_ORIGIN_MEDIA: "/short-forms/origin-media",
 
   // 모니터링 페이지
   INGEST_JOBS: "/ingest-jobs",
